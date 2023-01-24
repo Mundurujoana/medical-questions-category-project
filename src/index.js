39daeff86caa94d28467e13ce9dc165d13149eb2
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2023-01-24 15:31:44 +0200

    Fech data from firebse

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Dashboard from './components/Dashboard';
@@ -11,7 +11,6 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
-        <Route exact path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>,
   </React.StrictMode>,
