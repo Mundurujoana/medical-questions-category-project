13c45f7d28e1d8fc2af9cafc0e0f5e14c1db771a
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2023-01-25 11:07:18 +0200

    Fixed a form state issue

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,41 +1,43 @@
 import React, { useState } from 'react';
 
 function Forms() {
-const [newQuestion,setNewQuestion] = useState({
-  category:"",
-  question:"",
-})
+  const [changeCategory,setChangeCategory] = useState("");
+  const [newQuestion, setNewQuestion] = useState({
+    category: "",
+    question: "",
+  })
 
-const handleChange=(e)=>{
-setNewQuestion({
-  ...newQuestion,
-  [e.target.name]: e.target.value
-})
-}
+  const handleChange = (e) => {
+    setNewQuestion({
+      ...newQuestion,
+      category:changeCategory,
+      question: e.target.value
+    })
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  console.log(newQuestion)
+    console.log(newQuestion)
   }
 
 
   return (
     <form onSubmit={handleSubmit}>
-    <label>
-    Select a category:
-    <select value={newQuestion.category} onChange={(e) => {
-      handleChange(e)
-    }}>
-      <option value="">Select a category</option>
-      <option value="health">Health</option>
-      <option value="education">Education</option>
-      <option value="sports">Sports</option>
-    </select>
-  </label>
+      <label>
+        Select a category:
+        <select onChange={(e) => {
+          setChangeCategory(e.target.value);
+        }}>
+          <option value="" >Select a category</option>
+          <option  value="health">Health</option>
+          <option  value="education">Education</option>
+          <option  value="sports">Sports</option>
+        </select>
+      </label>
       <br />
       <label>
         Question:
-        <input type="text" name='question' value={newQuestion.question} onChange={e => setNewQuestion(e.target.value)} />
+        <input type="text" name='question' value={newQuestion.question} onChange={handleChange} />
       </label>
       <br />
       <button type="submit">Add</button>
