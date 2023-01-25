e41ce61b37b5fa9a1da8036996a0784bcba64977
Mundurujoana <mundujoana@gmail.com>
2023-01-25 11:29:31 +0200

    Fix conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
-import Main from './components/Main';
+import './App.css';
+import Forms from './Forms';
+import Main from "./components/Main";
 
 
 function App() {
     return (
       
     <div className="App">
-     <Main />
+     {/* <Main /> */}
+     <Forms />
+
     </div>
   )
 }
